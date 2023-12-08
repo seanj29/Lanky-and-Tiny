f1a2342b55dc73ed88c59d854ef0c7a3f365178c
seanj29 <seanotto29@gmail.com>
2023-12-08 22:02:16 +0000

    Updated lanky to be animatable

--- Assets/Player Characters/LankyScript.cs
+++ Assets/Player Characters/LankyScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LankyScript : MonoBehaviour
 {
 
+    public float rotateSpeed = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,6 @@ public class LankyScript : MonoBehaviour
     void Update()
     {
         
-        
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
     }
 }
