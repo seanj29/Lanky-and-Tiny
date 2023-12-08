1347c6836aaf5da812d0bc70ac053c77c65ed590
seanj29 <seanotto29@gmail.com>
2023-12-08 20:54:34 +0000

    Added editor settings and changed position of folder

--- Assets/Player Characters/LankyScript.cs
+++ Assets/Player Characters/LankyScript.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class LankyScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         
     }
 }
