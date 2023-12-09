4dfe23c7bfb5588d3f42188cb812774a5582e826
seanj29 <seanotto29@gmail.com>
2023-12-09 00:09:55 +0000

    added terrain

--- Assets/Player Characters/LankyScript.cs
+++ Assets/Player Characters/LankyScript.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class LankyScript : MonoBehaviour
 {
+
 
     public float rotateSpeed = 20;
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("Hello World");
+       //Fetch the SpriteRenderer from the GameObject
+        SpriteResolver m_spriteResolver = GetComponent<SpriteResolver>();
+        Rigidbody2D m_rigidBody2D = GetComponent<Rigidbody2D>();
+        string CurrentAnimationGroup = m_spriteResolver.GetCategory();
+        if (CurrentAnimationGroup != "LankyIdle")
+        {
+            m_spriteResolver.SetCategoryAndLabel("LankyIdle", "LankyIdle");
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+       
     }
 }
