af4ad5c852bfc86475ac8d7cdd5b6fecf6a6d618
seanj29 <seanotto29@gmail.com>
2023-12-09 01:11:26 +0000

    add movement maps

--- Assets/Player Characters/LankyScript.cs
+++ Assets/Player Characters/LankyScript.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.U2D.Animation;
 
 public class LankyScript : MonoBehaviour
 {
-
-
-    public float rotateSpeed = 20;
+    SpriteResolver m_spriteResolver;
+    Rigidbody2D m_rigidBody2D;
+    public Vector2 movementForce;
+    public InputAction move;
+    public InputAction jump;
     // Start is called before the first frame update
     void Start()
     {
+        move.Enable();
+        jump.Enable();
        //Fetch the SpriteRenderer from the GameObject
-        SpriteResolver m_spriteResolver = GetComponent<SpriteResolver>();
-        Rigidbody2D m_rigidBody2D = GetComponent<Rigidbody2D>();
+        m_spriteResolver = GetComponent<SpriteResolver>();
+        m_rigidBody2D = GetComponent<Rigidbody2D>();
+        
+
         string CurrentAnimationGroup = m_spriteResolver.GetCategory();
         if (CurrentAnimationGroup != "LankyIdle")
         {
@@ -24,7 +31,12 @@ public class LankyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movementForce = move.ReadValue<Vector2>();
         
-       
+        if (move.ReadValue<Vector2>() != Vector2.zero){
+        m_rigidBody2D.AddForce(movementForce);
+        Debug.Log(move.ReadValue<Vector2>());
+        }
     }
+    
 }
